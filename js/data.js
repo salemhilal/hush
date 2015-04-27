0409468c1f6313e111edd9da5ad3db6bb0053f6c
Salem <salemhilal@gmail.com>
2015-04-26 21:25:41 -0400

    Starting to add data, modals

--- js/data.js
+++ js/data.js
@@ -18,21 +18,22 @@
             imgUrl: 'img/instagram.png',
         },
         {
-            name: 'Facebook',
-            imgUrl: 'img/facebook.png',
+            name: 'Timehop',
+            imgUrl: 'img/timehop.jpg',
+            issues: ['Digest notifications'],
+            writeup: "Timehop is a utility app, but it sends daily notifications to check the timehop they’ve created. It’s not important to be notified about this, so we recommend turning notifications off in the general settings.",
         },
         {
-            name: 'Twitter',
-            imgUrl: 'img/twitter.png',
+            name: 'Words with Friends',
+            imgUrl: 'img/wordswithfriends.jpg',
+            issues: ['Excessive notifications', 'Irrelevant notifications', 'Ad-like notifications'],
+            writeup: "Timehop is a utility app, but it sends daily notifications to check the timehop they’ve created. It’s not important to be notified about this, so we recommend turning notifications off in the general settings.",
         },
         {
-            name: 'Pinterest',
-            imgUrl: 'img/pinterest.png'
-        },
-        {
-            name: 'Instagram',
-            imgUrl: 'img/instagram.png',
-        },
+            name: 'Shazam',
+            imgUrl: 'img/shazam.jpg',
+            issues: ['Ad-like notifications', 'Out-of-network notifications']
+        }
     ];
 })(window);
 console.log('hi');
--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@
     app.controller('hushController', function ($scope, $window) {
         console.log('Hush is running', $window.apps);
         $scope.apps = $window.apps;
+
+        $scope.modalApp = $scope.apps[0];
+
+        $scope.showModal = function(app) {
+            $scope.modalApp = app;
+            $('body').addClass('freeze');
+            $('.overlay').fadeIn();
+            $('.modal').animate({marginTop: '60px'});
+        };
+
+        $scope.hideModal = function() {
+            $('.overlay').fadeOut();
+            $('.modal').animate({marginTop: '30px'});
+            $('body').removeClass('freeze');
+        };
     });
 
 })();
